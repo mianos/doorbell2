c96ac8ae1a32ac3b73505d2936baef53d0be6e3e
Rob Fowler <rfo@mianos.com>
2023-12-28 18:37:18 +1100

    first

---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+/**
+ * @file player-url-i2s.ino
+ * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-player/player-url-i2s/README.md
+ * 
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+
+
+#include "AudioTools.h"
+#include "AudioCodecs/CodecMP3Helix.h"
+#include "provision.h"
+
+const char *urls[] = {
+  "http://stream.srg-ssr.ch/m/rsj/mp3_128",
+  "http://stream.srg-ssr.ch/m/drs3/mp3_128",
+  "http://stream.srg-ssr.ch/m/rr/mp3_128",
+  "http://sunshineradio.ice.infomaniak.ch/sunshineradio-128.mp3",
+  "http://streaming.swisstxt.ch/m/drsvirus/mp3_128"
+};
+const char *wifi = "iot";
+const char *password = "iotlongpassword";
+
+WiFiClient wifiClient;
+// URLStream urlStream(wifi, password);
+URLStream urlStream("", "");
+AudioSourceURL source(urlStream, urls, "audio/mp3");
+I2SStream i2s;
+MP3DecoderHelix decoder;
+AudioPlayer player(source, i2s, decoder);
+
+// additional controls
+const int volumePin = A0;
+Debouncer nextButtonDebouncer(2000);
+const int nextButtonPin = 0;
+
+void setup() {
+  Serial.begin(115200);
+  wifi_connect();
+  urlStream.setClient(wifiClient);
+  AudioLogger::instance().begin(Serial, AudioLogger::Error);
+
+// pinMode(nextButtonPin, INPUT_PULLUP);
+
+  // setup output
+  auto cfg = i2s.defaultConfig(TX_MODE);
+#if defined(SEEED)
+  cfg.pin_data = D2;
+  cfg.pin_bck = D1;
+  cfg.pin_ws = D0;
+
+#else
+  cfg.pin_data = 27;
+  cfg.pin_bck = 26;
+  cfg.pin_ws = 25;
+#endif
+  i2s.begin(cfg);
+
+  // setup player
+  player.begin();
+}
+
+// Sets the volume control from a linear potentiometer input
+void updateVolume() {
+  // Reading potentiometer value (range is 0 - 4095)
+  float vol = static_cast<float>(analogRead(volumePin));
+  // min in 0 - max is 1.0
+  player.setVolume(vol/4095.0);
+}
+
+
+// Moves to the next url when we touch the pin
+void updatePosition() {
+   if (digitalRead(nextButtonPin) == LOW) {
+      Serial.println("Moving to next url");
+      if (nextButtonDebouncer.debounce()){
+        player.next();
+      }
+  }
+}
+
+
+void loop() {
+  //updateVolume(); // remove comments to activate volume control
+  // updatePosition();  // remove comments to activate position control
+  player.copy();
+}
---
+++ src/provision.cpp
@@ -0,0 +1,104 @@
+#include "WiFiProv.h"
+#include "WiFi.h"
+
+int prov_on_flag = 0;
+int prov_end = 0;
+int prov_err = 0;
+
+void SysProvEvent(arduino_event_t *sys_event)
+{
+    switch (sys_event->event_id) {
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+        Serial.print("\nConnected IP address : ");
+        Serial.println(IPAddress(sys_event->event_info.got_ip.ip_info.ip.addr));
+        break;
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+        Serial.println("\nWHAT?  Disconnected. Connecting to the AP again... ");
+        break;
+    case ARDUINO_EVENT_PROV_START:
+        Serial.println("\nProvisioning started\nGive Credentials of your access point using \" Android app \"");
+        break;
+    case ARDUINO_EVENT_PROV_CRED_RECV: 
+        Serial.println("\nReceived Wi-Fi credentials");
+        Serial.print("\tSSID : ");
+        Serial.println((const char *) sys_event->event_info.prov_cred_recv.ssid);
+        Serial.print("\tPassword : ");
+        Serial.println((char const *) sys_event->event_info.prov_cred_recv.password);
+        break;
+    case ARDUINO_EVENT_PROV_CRED_FAIL: 
+        Serial.println("\nProvisioning failed!\nPlease reset to factory and retry provisioning\n");
+        if(sys_event->event_info.prov_fail_reason == WIFI_PROV_STA_AUTH_ERROR) 
+            Serial.println("\nWi-Fi AP password incorrect");
+        else
+            Serial.println("\nWi-Fi AP not found....Add API \" nvs_flash_erase() \" before beginProvision()");        
+        break;
+    case ARDUINO_EVENT_PROV_CRED_SUCCESS:
+        Serial.println("\nProvisioning Successful");
+        break;
+    case ARDUINO_EVENT_PROV_END:
+        Serial.println("\nProvisioning Ends");
+        break;
+    case ARDUINO_EVENT_PROV_INIT:
+        Serial.printf("Prov: Init\n");
+        break;
+    case ARDUINO_EVENT_PROV_DEINIT:
+        Serial.printf("Prov: Stopped\n");
+        break;
+    case ARDUINO_EVENT_WIFI_SCAN_DONE:
+        printf("Prov: scan done\n");
+        break;
+    case SYSTEM_EVENT_STA_START:
+        printf("Station start\n");
+        break;
+    case SYSTEM_EVENT_STA_CONNECTED:
+        printf("Station connected\n");
+        break;
+    default:
+        Serial.printf("Some other event %d\n", sys_event->event_id);
+        break;
+    }
+}
+
+#if defined(BOOT_BUTTON)
+constexpr int PROG_BUTTON_PIN = BOOT_BUTTON;
+#else
+constexpr int PROG_BUTTON_PIN = 0; // GPIO0
+#endif
+
+String getMacAddress() {
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  char macStr[18] = { 0 };
+  sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X",  mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  return String(macStr);
+}
+
+void wifi_connect() {
+  WiFi.mode(WIFI_STA);
+  WiFi.onEvent(SysProvEvent);
+  WiFi.disconnect();  // Disconnect from the WiFi
+  Serial.printf("mac '%s'\n", getMacAddress().c_str());
+  String name = String("faba_");
+  name += String(random(0xffff), HEX);
+  printf("prov name: %s\n",name.c_str());
+
+  Serial.printf("press and hold prog now to reset .... \n");
+  delay(1000);
+/*  if (digitalRead(PROG_BUTTON_PIN) == LOW) {
+    Serial.printf("Reset button low Resetting\n");
+    WiFi.disconnect(false,true); 
+    delay(500);
+    ESP.restart();
+  } */
+  Serial.printf("Begin Provisioning using Soft AP\n");
+  const char * pop = "abcd1234"; 
+  const char * service_name = "PROV_PWM";
+  const char * service_key = NULL; 
+
+  WiFiProv.beginProvision(WIFI_PROV_SCHEME_SOFTAP, WIFI_PROV_SCHEME_HANDLER_NONE, WIFI_PROV_SECURITY_1, pop, service_name);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf("Connecting to WiFi...\n");
+    delay(2000);
+  }
+  Serial.printf("COnnected\n");
+}
